fb17f24fee5fc1943f0aa8ba663cda3ed067df5d
Abhishek-2502 <abhishek25022004@gmail.com>
2025-10-03 15:30:37 +0530

    Dockerized app

--- Url-Shortner-Frontend/vite.config.js
+++ Url-Shortner-Frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // allow external access (inside Docker)
+    port: 5173,        // make sure this matches EXPOSE in Dockerfile
+    strictPort: true,  // fail if port is taken, instead of picking a random one
+    watch: {
+      usePolling: true // needed for hot reload in Docker sometimes
+    },
+    allowedHosts: ['.localhost', '.localdomain', 'your-domain.com'] 
+  }
 })
